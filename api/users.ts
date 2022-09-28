ed757cd7878fd437a1cf4b8a17cf3a854c9756a1
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-09-28 15:47:14 -0400

    Use require instead of import

--- api/users.ts
+++ api/users.ts
@@ -1,8 +1,8 @@
 // https://vercel.com/docs/runtimes#official-runtimes/node-js/using-type-script-with-the-node-js-runtime
 import type {VercelRequest, VercelResponse} from '@vercel/node'
-import path from 'path'
-import fs from 'fs-extra'
-import fsOriginal from 'fs'
+const path = require("path");
+const fsOriginal = require("fs");
+const fs = require("fs-extra");
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   console.log('path module:', path)
