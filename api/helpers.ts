cceb529d828f7293a1864f510b48bd230f7d9f3d
Qodesmith <theqodesmith@gmail.com>
2022-09-29 10:10:00 -0400

    Adding support for fetching single user.

--- api/helpers.ts
+++ api/helpers.ts
@@ -5,3 +5,29 @@ export function fakeAdd(num1: number, num2: number) {
 export function fakeHello(name?: string) {
   return `Hello ${name ?? 'unknown'}!`
 }
+
+type DateData = {month: number; day: number; year: number}
+
+export function createLocaleDate({month, day, year}: DateData) {
+  return new Date(year, month, day).toLocaleDateString()
+}
+
+export function createDateString({month, day, year}: DateData) {
+  return new Date(year, month, day).toDateString()
+}
+
+export function errorToObject(err?: Error): Record<string, string> | null {
+  if (!err || !(err instanceof Error)) return null
+
+  const obj: Record<string, string> = {}
+
+  Object.getOwnPropertyNames(err).forEach(prop => {
+    const value = err[prop as keyof Error]
+
+    if (typeof value !== 'string') return
+
+    obj[prop] = value
+  })
+
+  return obj
+}
--- api/users/[id].ts
+++ api/users/[id].ts
@@ -1,14 +1,6 @@
 // https://vercel.com/docs/runtimes#official-runtimes/node-js/using-type-script-with-the-node-js-runtime
 import type {VercelRequest, VercelResponse} from '@vercel/node'
-
-// `import` syntax works for local modules.
-import {
-  fakeAdd,
-  fakeHello,
-  createLocaleDate,
-  createDateString,
-  errorToObject,
-} from './helpers'
+import {errorToObject, createLocaleDate, createDateString} from '../helpers'
 
 /*
   https://github.com/vercel/community/discussions/893#discussioncomment-3756470
@@ -19,9 +11,6 @@ const fs = require('fs-extra')
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    console.log('2 + 3:', fakeAdd(2, 3))
-    console.log('Hello:', fakeHello('world'))
-
     // https://vercel.com/guides/how-can-i-use-files-in-serverless-functions
     const pathFromProjectRootToFile = '/api/usersData.json'
     const usersDataFilePath = path.join(
@@ -29,14 +18,14 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
       pathFromProjectRootToFile
     )
     const usersData = fs.readJSONSync(usersDataFilePath)
-    const usersDataWithDates = usersData.map(({dob, ...rest}) => ({
-      ...rest,
-      dob,
-      localeDate: createLocaleDate(dob),
-      dateString: createDateString(dob),
-    }))
+    const user = usersData.find(user => user.id.toString() === req.query.id)
 
-    res.json({users: usersDataWithDates})
+    if (user) {
+      user.localeDate = createLocaleDate(user.dob)
+      user.dateString = createDateString(user.dob)
+    }
+
+    res.json(user)
   } catch (e) {
     res.json({error: errorToObject(e as Error)})
   }
