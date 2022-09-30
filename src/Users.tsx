147907837d711a78081640e97da984dbb4cb2c2c
Qodesmith <theqodesmith@gmail.com>
2022-09-30 19:24:11 -0400

    Rendering users.

--- src/Users.tsx
+++ src/Users.tsx
@@ -4,7 +4,23 @@ import {useMatch} from '@tanstack/react-location'
 
 export default function Users() {
   const {data} = useMatch<LocationGenerics>()
-  console.log(data)
 
-  return <h1>Users!</h1>
+  if (data.users) {
+    return (
+      <div>
+        {data.users.map(({id, firstName, lastName, dob}) => {
+          const date = new Date(dob.year, dob.month, dob.day)
+
+          return (
+            <div key={id} className="user">
+              {firstName} {lastName}
+              <div>Birthday: {date.toLocaleDateString()}</div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  return <div>No users found</div>
 }
