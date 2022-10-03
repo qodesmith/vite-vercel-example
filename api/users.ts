c6cb3c5e1dcca3a6129307ba5c855cfdc51455c6
Qodesmith <theqodesmith@gmail.com>
2022-10-03 10:49:59 -0400

    Exporting type

--- api/users.ts
+++ api/users.ts
@@ -1,6 +1,17 @@
 // https://vercel.com/docs/runtimes#official-runtimes/node-js/using-type-script-with-the-node-js-runtime
 import type {VercelRequest, VercelResponse} from '@vercel/node'
 
+export type UserType = {
+  id: number
+  firstName: string
+  lastName: string
+  dob: {
+    month: number
+    day: number
+    year: number
+  }
+}
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
   res.json({users: [], message: 'this file is found at /api/users.ts'})
 }
